fcd801818dc3ba35f45c76ed67797efcb7d140df
nathanpeter636 <nathanpeter636@gmail.com>
2021-05-07 11:50:29 +1200

    edited proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import { setContext } from 'apollo-link-context';
 
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://git.heroku.com/socialize-build.git'
   });
   
   const authLink = setContext(() => {
